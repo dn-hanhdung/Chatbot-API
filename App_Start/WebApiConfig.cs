15385ee0caac43ec6b3481109874fe09992eba54
1753021 <51194229+1753021@users.noreply.github.com>
2021-01-12 21:35:35 +0700

    Edit route part 2

--- App_Start/WebApiConfig.cs
+++ App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace IT_Career_Counseling
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "https://itcareercounseling.azurewebsites.net/{controller}/{id}",
+                routeTemplate: "itcareercounseling.azurewebsites.net/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
