4e9db64b5f933e8437310399e0420d5fcc8fc5ca
1753021 <51194229+1753021@users.noreply.github.com>
2021-01-12 21:18:26 +0700

    Basic API
    1 route: /api/test
    input: request body, no parameter
    output: String of request body

---
+++ Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace IT_Career_Counseling.Controllers
+{
+    public class TestController : ApiController
+    {
+        [Route("")]
+        public String GetContent()
+        {
+            string req_txt;
+            using (StreamReader reader = new StreamReader(HttpContext.Current.Request.InputStream))
+            {
+                req_txt = reader.ReadToEnd();
+            }
+
+            return req_txt;
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IT_Career_Counseling.WebApiApplication" Language="C#" %>
